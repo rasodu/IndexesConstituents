a434ff87c9f2d90a987ecb3fa35ba95aa2b2ec83
Abhijit Amin <abhijitramin@gmail.com>
2018-04-12 04:09:04 -0400

    Wiki page scraper.

--- src/Rasodu.EquityIndexes/Equity.cs
+++ src/Rasodu.EquityIndexes/Equity.cs
@@ -1,8 +1,25 @@
-﻿namespace Rasodu.EquityIndexes
+﻿using System;
+
+namespace Rasodu.EquityIndexes
 {
-    internal class Equity
+    internal class Equity : IComparable<Equity>, IEquatable<Equity>
     {
         internal string StockExchange = null;
         internal string Identifier = null;
+        public int CompareTo(Equity other)
+        {
+            var compareVal = StockExchange.CompareTo(other.StockExchange);
+            if (compareVal == 0)
+            {
+                compareVal = Identifier.CompareTo(other.Identifier);
+            }
+            return compareVal;
+        }
+        public bool Equals(Equity other)
+        {
+            return
+                StockExchange == other.StockExchange
+                && Identifier == other.Identifier;
+        }
     }
 }
