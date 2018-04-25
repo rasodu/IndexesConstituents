091c6a5d151135cf8bd6d8e58b827a1222dd9b52
Abhijit Amin <abhijitramin@gmail.com>
2018-04-25 17:54:21 -0400

    Add json writer.

---
+++ src/Rasodu.EquityIndexes.Test/JSONEquityIndexStoreTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Rasodu.EquityIndexes.Test
+{
+    public class JSONEquityIndexStoreTest
+    {
+        [Fact]
+        public void ReplaceAllTest()
+        {
+            //arrange
+            var expectedJSONText = "[\n  {\n    \"StockExchange\": \"NYSE\",\n    \"Identifier\": \"AXP\"\n  },\n  {\n    \"StockExchange\": \"NYSE\",\n    \"Identifier\": \"MMM\"\n  }\n]";
+            var equityList = new List<Equity>()
+            {
+                new Equity
+                {
+                    StockExchange = "NYSE",
+                    Identifier = "AXP",
+                },
+                new Equity
+                {
+                    StockExchange = "NYSE",
+                    Identifier = "MMM",
+                }
+            };
+            TextWriter writer = new StringWriter();
+            var equityStore = new JSONEquityIndexStore(writer);
+            //act
+            equityStore.ReplaceAll(equityList);
+            var actualJSONText = writer.ToString();
+            //assert
+            Assert.Equal(expectedJSONText, actualJSONText);
+        }
+    }
+}
---
+++ src/Rasodu.EquityIndexes/JSONEquityIndexStore.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Rasodu.EquityIndexes
+{
+    class JSONEquityIndexStore : IEquityIndexStore
+    {
+        private TextWriter _destination;
+        internal JSONEquityIndexStore(TextWriter destination)
+        {
+            _destination = destination;
+            _destination.NewLine = "\n";
+        }
+        public void ReplaceAll(List<Equity> equities)
+        {
+            var jsonWriter = new JsonTextWriter(_destination);
+            jsonWriter.Formatting = Formatting.Indented;
+            var jsonSerializer = new JsonSerializer();
+            jsonSerializer.Serialize(jsonWriter, equities);
+        }
+    }
+}
