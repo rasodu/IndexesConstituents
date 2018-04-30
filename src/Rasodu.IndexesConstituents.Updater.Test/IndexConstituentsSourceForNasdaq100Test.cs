d25c2dd568c8c9bccbf5eb7bbcf521243f06661f
Abhijit Amin <abhijitramin@gmail.com>
2018-04-30 17:11:23 -0400

    Add Nasdaq 100 index.

---
+++ src/Rasodu.IndexesConstituents.Updater.Test/IndexConstituentsSourceForNasdaq100Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace Rasodu.IndexesConstituents.Updater.Test
+{
+    public class IndexConstituentsSourceForNasdaq100Test
+    {
+        [Fact]
+        public void GetAllEquitiesTest()
+        {
+            //arrange
+            var expectedConstituents = new List<Equity>
+            {
+                new Equity
+                {
+                    StockExchange = "NASDAQ",
+                    Identifier = "ADBE",
+                },
+                new Equity
+                {
+                    StockExchange = "NASDAQ",
+                    Identifier = "ATVI",
+                },
+            };
+            var csvContent = @"
+Symbol, Name, lastsale, netchange,pctchange, share_volume, Nasdaq100_points,
+ATVI, Activision Blizzard Inc, 66.35, 0.56,0.85, 5039691, .4,
+ADBE, Adobe Systems Incorporated, 221.6, -0.3,-0.14, 1761740, -.1,
+";
+            TextReader csvTextReader = new StringReader(csvContent);
+            var nify100Source = new IndexConstituentsSourceForNasdaq100(csvTextReader);
+            //act
+            var actualEquityList = nify100Source.GetAllEquities();
+            //assert
+            Assert.Equal(expectedConstituents, actualEquityList);
+        }
+    }
+}
---
+++ src/Rasodu.IndexesConstituents.Updater/IndexConstituentsSourceForNasdaq100.cs
@@ -0,0 +1,52 @@
+﻿using CsvHelper;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Rasodu.IndexesConstituents.Updater
+{
+    public class Nasdaq100Entry
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string lastsale { get; set; }
+        public string netchange { get; set; }
+        public string pctchange { get; set; }
+        public string share_volume { get; set; }
+        public string Nasdaq100_points { get; set; }
+    }
+    class IndexConstituentsSourceForNasdaq100 : IIndexConstituentsSource
+    {
+        private TextReader _csvTextReader;
+        public IndexConstituentsSourceForNasdaq100()
+        {
+            _csvTextReader = Helper.UrlToTextReader("https://www.nasdaq.com/quotes/nasdaq-100-stocks.aspx?render=download");
+        }
+        public IndexConstituentsSourceForNasdaq100(TextReader csvTextReader)
+        {
+            _csvTextReader = csvTextReader;
+        }
+        public List<Equity> GetAllEquities()
+        {
+            var returnEquityList = new List<Equity>();
+            var csvHelperReader = new CsvReader(_csvTextReader);
+            csvHelperReader.Configuration.HasHeaderRecord = true;
+            csvHelperReader.Configuration.PrepareHeaderForMatch = header => header.Replace(" ", "");
+            var recordList = csvHelperReader.GetRecords<Nasdaq100Entry>();
+            foreach (var record in recordList)
+            {
+                var equity = new Equity
+                {
+                    StockExchange = "NASDAQ",
+                    Identifier = record.Symbol,
+                };
+                returnEquityList.Add(equity);
+            }
+            returnEquityList.Sort();
+            return returnEquityList;
+        }
+        public string IndexName()
+        {
+            return "Nasdaq100";
+        }
+    }
+}
