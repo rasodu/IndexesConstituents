6996afe1ffdced91c771dfb962295f7d951eac71
Abhijit Amin <abhijitramin@gmail.com>
2019-05-28 17:44:33 -0400

    Fix S&P 500 scanner

--- src/Rasodu.IndexesConstituents.Updater.Test/IndexConstituentsSourceForSP500Tests.cs
+++ src/Rasodu.IndexesConstituents.Updater.Test/IndexConstituentsSourceForSP500Tests.cs
@@ -12,17 +12,24 @@ namespace Rasodu.IndexesConstituents.Updater.Test
             //arrange
             var expectedWikipage = @"
 more text here-->
-<table class=""wikitable sortable"">
+<table class=""wikitable sortable"" id=""constituents"">
+
+
 <tr>
-<th><a href=""/wiki/Ticker_symbol"" title=""Ticker symbol"">Ticker symbol</a></th>
+<th><a href=""/wiki/Symbol"" title=""Symbol"">Symbol</a></th>
 <th>Security</th>
 <th><a href=""/wiki/SEC_filing"" title=""SEC filing"">SEC filings</a></th>
 <th><a href=""/wiki/Global_Industry_Classification_Standard"" title=""Global Industry Classification Standard"">GICS</a> Sector</th>
 <th>GICS Sub Industry</th>
-<th>Address of Headquarters</th>
-<th>Date first added<sup id=""cite_ref-3"" class=""reference""><a href=""#cite_note-3"">[3]</a></sup><sup id=""cite_ref-4"" class=""reference""><a href=""#cite_note-4"">[4]</a></sup></th>
+<th>Headquarters Location</th>
+<th>Date first added</th>
 <th><a href=""/wiki/Central_Index_Key"" title=""Central Index Key"">CIK</a></th>
+<th>Founded</th>
 </tr>
+
+<tbody>
+
+
 <tr>
 <td><a rel=""nofollow"" class=""external text"" href=""https://www.nyse.com/quote/XNYS:MMM"">MMM</a></td>
 <td><a href=""/wiki/3M"" title=""3M"">3M Company</a></td>
@@ -32,6 +39,7 @@ more text here-->
 <td><a href=""/wiki/St._Paul,_Minnesota"" class=""mw-redirect"" title=""St. Paul, Minnesota"">St. Paul, Minnesota</a></td>
 <td></td>
 <td>0000066740</td>
+<td>1902</td>
 </tr>
 <tr>
 <td><a rel=""nofollow"" class=""external text"" href=""https://www.nyse.com/quote/XNYS:ABT"">ABT</a></td>
@@ -42,7 +50,11 @@ more text here-->
 <td><a href=""/wiki/North_Chicago,_Illinois"" title=""North Chicago, Illinois"">North Chicago, Illinois</a></td>
 <td>1964-03-31</td>
 <td>0000001800</td>
+<td>1888</td>
 </tr>
+
+</tbody>
+
 </table>
 <--more text here
 ";
